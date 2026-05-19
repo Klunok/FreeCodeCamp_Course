2790ef7791475aab622fac9023b92b7733a81ebf
Valeriy <41185621+Veskel@users.noreply.github.com>
2026-05-19 17:47:01 +0300

    1

---
+++ 02-JavaScript_Certification/01-Variables_and_Strings/04-Build_a_JS_Trivia_Bot.js
@@ -0,0 +1,28 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName;
+botName = "botame";
+let botLocation;
+botLocation = "WebInternet";
+let favoriteLanguage;
+favoriteLanguage = "Programming Language";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact;
+
+codingFact = "a fun fact about bots " + favoriteLanguage + " .";
+
+console.log(codingFact);
+
+codingFact = "a fun fact about bots " + favoriteLanguage + " number two.";
+
+console.log(codingFact);
+
+codingFact = "a fun fact about bots " + favoriteLanguage + " number three.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
